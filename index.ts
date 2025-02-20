48a8c9478ed6e8b6d7c1679adeef4f57ba19fe12
Avinash sharma <sharmaavi2196@gmail.com>
2025-02-20 20:46:39 +0530

    Add string and number examples, and implement a simple function

--- index.ts
+++ index.ts
@@ -7,3 +7,21 @@ const person: Person = {
 };
 
 console.log(person.name);
+
+//string
+let myName: string = "sunil chetri";
+myName = "bhaichung bhutia";
+
+console.log(myName);
+
+//numbers
+let quantitiy: Number = 23;
+quantitiy = 56;
+console.log(quantitiy);
+
+//regular functions
+function addOne(num: number) {
+  return num + 1;
+}
+const res = addOne(4);
+console.log(res);
